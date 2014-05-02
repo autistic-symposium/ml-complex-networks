32d99605a48f4bea180b13c1a4f1a896fd918d62
Marina Wahl <mari.wahl9@gmail.com>
2014-05-02 16:44:48 -0400

    adaboost improved

---
+++ MLNet-TODO/supervisioned_learning/classifiers/adaboost/src/constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# marina von steinkirch @2014
+# steinkirch at gmail
+
+
+INPUT_FILE = ["sampled", "entire_net"]
+SAMPLINGS_SETS = [50, 100]
+PERCENTAGE = 0.9 # percentage for training and test sets
+
--- MLNet-TODO/supervisioned_learning/classifiers/adaboost/src/run_all.sh
+++ MLNet-TODO/supervisioned_learning/classifiers/adaboost/src/run_all.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 
-python adaboost_main_bio.py
-python adaboost_main_info.py
-python adaboost_main_tech.py
-python adaboost_main_social.py
-adaboost_main_bio_first10.py
-adaboost_main_tech_first10.py
-adaboost_main_info_first10.py
-adaboost_main_social_first10.py
+python ./src/main.py
